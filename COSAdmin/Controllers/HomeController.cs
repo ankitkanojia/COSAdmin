89b5cd249760ffcba56df870eb0e6a844aa6f966
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-10 15:04:01 +0530

    Added login action into home screen

--- COSAdmin/Controllers/HomeController.cs
+++ COSAdmin/Controllers/HomeController.cs
@@ -8,22 +8,8 @@ namespace COSAdmin.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Login()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
