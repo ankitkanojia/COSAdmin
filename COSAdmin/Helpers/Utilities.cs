055a3d0f7cf1e4619ac5a780b7204f5607c19bfe
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-12 18:46:16 +0530

    Added dropdown value fetch functionality.
    Added functionality of fetching the data from database so that it will work as dynamic binding on view side.

--- COSAdmin/Helpers/Utilities.cs
+++ COSAdmin/Helpers/Utilities.cs
@@ -1,13 +1,100 @@
-﻿using System;
+﻿using COSAdmin.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace COSAdmin.Helpers
 {
     public class Utilities
     {
+        public static List<SelectListItem> GetClubServices()
+        {
+            List<SelectListItem> lstCategory = new List<SelectListItem>();
+
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var data = db.ClubServices.Where(s => s.IsActive).ToList();
+
+                    lstCategory = data.Select(s => new SelectListItem
+                    {
+                        Text = s.ServiceName,
+                        Value = s.ClubServiceID.ToString()
+
+                    }).ToList();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return lstCategory;
+        }
+
+        public static List<SelectListItem> GetClubs()
+        {
+            List<SelectListItem> lstCategory = new List<SelectListItem>();
+
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var data = db.ClubMasters.Where(s => s.IsActive).ToList();
+
+                    lstCategory = data.Select(s => new SelectListItem
+                    {
+                        Text = s.ClubName,
+                        Value = s.ClubMasterID.ToString()
+
+                    }).ToList();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return lstCategory;
+        }
+
+        public static List<SelectListItem> GetCouchList()
+        {
+            List<SelectListItem> lstCategory = new List<SelectListItem>();
+
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    SelectListItem single = new SelectListItem();
+                    single.Value = "";
+                    single.Text = "Select service coach";
+
+                    lstCategory.Add(single);
+
+                    var data = db.CoachMasters.Where(s => s.IsActive).ToList();
+
+                    lstCategory.AddRange(data.Select(s => new SelectListItem
+                    {
+                        Text = s.FirstName + " " + s.LastName,
+                        Value = s.CouchMasterID.ToString()
+
+                    }).ToList());
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return lstCategory;
+        }
+
+
         public static string SaveImage(HttpPostedFileBase file, string path)
         {
             try
