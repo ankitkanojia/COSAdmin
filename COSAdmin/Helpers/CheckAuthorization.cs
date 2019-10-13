c6c7486231fa463dac97bc4f60f4e67574a4ef4a
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-13 12:20:00 +0530

    CheckAuthorization function developed
    CheckAuthorization developed the function to verification or authentcation of User.

---
+++ COSAdmin/Helpers/CheckAuthorization.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COSAdmin.Helpers
+{
+    public class CheckAuthorization : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (string.IsNullOrWhiteSpace(CookieHelper.GetCookie("UserID")) || !HttpContext.Current.Request.IsAuthenticated)
+            {
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    filterContext.HttpContext.Response.StatusCode = 302; //Found Redirection to another page. Here- login page. Check Layout ajaxError() script.
+                    filterContext.HttpContext.Response.End();
+                }
+                else
+                {
+                    filterContext.Result = new RedirectResult(System.Web.Security.FormsAuthentication.LoginUrl + "?ReturnUrl=" +
+                         filterContext.HttpContext.Server.UrlEncode(filterContext.HttpContext.Request.RawUrl));
+                }
+            }
+            else
+            {
+
+                //Code HERE for page level authorization
+
+            }
+        }
+    }
+}
