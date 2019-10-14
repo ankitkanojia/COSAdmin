2c3f45890495797922dffd94129d53679f4d40bc
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-14 09:05:55 +0530

    Added Dashboard controller with admin dashboard action

---
+++ COSAdmin/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using COSAdmin.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COSAdmin.Controllers
+{
+    [CheckAuthorization]
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
