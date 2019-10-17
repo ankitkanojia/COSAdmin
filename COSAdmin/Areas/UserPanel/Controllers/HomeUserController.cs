3e67b9aecdcd9dbfaf874550693148c0ea8f86ce
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-17 19:35:10 +0530

    HomeUser controller included and its view page
    Integrate Home User controller and its view file.
    Set up layout of admin panel

---
+++ COSAdmin/Areas/UserPanel/Controllers/HomeUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COSAdmin.Areas.UserPanel.Controllers
+{
+    public class HomeUserController : Controller
+    {
+        // GET: UserPanel/HomeUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
