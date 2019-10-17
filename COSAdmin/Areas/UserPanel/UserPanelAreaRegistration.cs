1a1175d6ead85dffd73f2120ebf92094d7d44f65
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-17 19:31:47 +0530

    Added Admin area in the project
    Area defined for Admin into the project

---
+++ COSAdmin/Areas/UserPanel/UserPanelAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace COSAdmin.Areas.UserPanel
+{
+    public class UserPanelAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "UserPanel";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "UserPanel_default",
+                "UserPanel/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
