87132bc05c5d2ec41b9feb910a618f29f73c9d35
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-18 13:18:24 +0530

    Payment gateway integration
    Added payment gateway code.
    Common helper class added.

--- COSAdmin/Areas/UserPanel/Controllers/HomeUserController.cs
+++ COSAdmin/Areas/UserPanel/Controllers/HomeUserController.cs
@@ -132,5 +132,93 @@ namespace COSAdmin.Areas.UserPanel.Controllers
                 throw;
             }
         }
+
+        #region --> PayUMoney Payment Integration
+
+        public ActionResult Payment(long id = 0)
+        {
+            PayuResponse payuResponse = new PayuResponse();
+            UserMaster usermaster = new UserMaster();
+
+            try
+            {
+                using (db = new DBEntities())
+                {
+                    usermaster = db.UserMasters.Where(s => s.UserMasterID == id).FirstOrDefault();
+
+                    if (usermaster != null)
+                    {
+                        usermaster.IsPaid = true;
+                        db.Entry(usermaster).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+
+                        payuResponse.tblPKID = usermaster.UserMasterID;
+                        payuResponse.amount = "350";
+                    }
+
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return View(payuResponse);
+        }
+
+        [HttpPost]
+        public void Payment(PayuResponse data)
+        {
+
+            try
+            {
+                RemotePost myremotepost = new RemotePost();
+                string key = ConfigurationManager.AppSettings["MERCHANT_KEY"];
+                string salt = ConfigurationManager.AppSettings["SALT"];
+                string surl = ConfigurationManager.AppSettings["SUCC_URL"];
+                string furl = ConfigurationManager.AppSettings["FAIL_URL"];
+                string zipcode = "390025";
+                string productInfo = "test";
+
+                //posting all the parameters required for integration.
+                myremotepost.Url = ConfigurationManager.AppSettings["PAYU_BASE_URL"];
+                myremotepost.Add("key", key);
+                string txnid = Guid.NewGuid().ToString().Substring(12);
+                myremotepost.Add("txnid", txnid);
+                myremotepost.Add("amount", data.amount);
+                myremotepost.Add("productinfo", productInfo);
+                myremotepost.Add("firstName", data.firstName);
+                myremotepost.Add("lastname", data.lastname);
+                myremotepost.Add("email", data.email);
+                myremotepost.Add("phone", data.phone);
+                myremotepost.Add("address1", data.address1);
+                myremotepost.Add("address2", data.address2);
+                myremotepost.Add("city", data.city);
+                myremotepost.Add("state", data.state);
+                myremotepost.Add("zipcode", zipcode);
+                myremotepost.Add("surl", surl);
+                myremotepost.Add("furl", furl);
+                string hashString = key + "|" + txnid + "|" + data.amount + "|" + productInfo + "|" + data.firstName + "|" + data.email + "|||||||||||" + salt;
+                string hash = Hashing.Generatehash512(hashString);
+                myremotepost.Add("hash", hash);
+                myremotepost.Post();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public ActionResult Success(PayuResponse data)
+        {
+            return View();
+        }
+
+        public ActionResult Fail(PayuResponse data)
+        {
+            return View();
+        }
+
+        #endregion
     }
 }
--- COSAdmin/Models/ViewModels/HomeVM.cs
+++ COSAdmin/Models/ViewModels/HomeVM.cs
@@ -5,6 +5,62 @@ using System.Web;
 
 namespace COSAdmin.Models.ViewModels
 {
+    public class PayuResponse
+    {
+        public string firstName { get; set; }
+        public string lastname { get; set; }
+        public string amount { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public long tblPKID { get; set; }
+        public string mihpayid { get; set; }
+        public string mode { get; set; }
+        public string status { get; set; }
+        public string txnid { get; set; }
+        public string Error { get; set; }
+        public string key { get; set; }
+        public string discount { get; set; }
+        public string offer { get; set; }
+        public string productinfo { get; set; }
+        public string country { get; set; }
+        public string udf1 { get; set; }
+        public string udf2 { get; set; }
+        public string udf3 { get; set; }
+        public string udf4 { get; set; }
+        public string udf5 { get; set; }
+        public string Hash { get; set; }
+        public string Errorbankcode { get; set; }
+        public string PG_TYPE { get; set; }
+        public string bank_ref_num { get; set; }
+        public string shipping_firstname { get; set; }
+        public string shipping_lastname { get; set; }
+        public string shipping_address1 { get; set; }
+        public string shipping_address2 { get; set; }
+        public string shipping_city { get; set; }
+        public string shipping_state { get; set; }
+        public string shipping_country { get; set; }
+        public string shipping_zipcode { get; set; }
+        public string shipping_phone { get; set; }
+        public string unmappedstatus { get; set; }
+        public bool IsSuccess { get; set; }
+
+    }
+
+    public class RegistrationVM
+    {
+        public string Mobile { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long CityMasterID { get; set; }
+        public string Address { get; set; }
+        public long ClubMasterID { get; set; }
+    }
     public class LoginVM
     {
         public string Mobile { get; set; }
@@ -12,6 +68,14 @@ namespace COSAdmin.Models.ViewModels
         public string Password { get; set; }
 
         public bool IsAdmin { get; set; }
+
+    }
+
+    public class ResetPWDGetVM
+    {
+        public string UniqueCode { get; set; }
+
+        public string Mobile { get; set; }
 
     }
 
