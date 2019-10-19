caf240810019322489e46eae5176b37ae6c4d147
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-19 14:50:27 +0530

    Club user module implementation
    Configured controller for club user on admin area.
    Create SelectClub for selection of club functionality is created

---
+++ COSAdmin/Areas/UserPanel/Controllers/ClubUserController.cs
@@ -0,0 +1,33 @@
+﻿using COSAdmin.Models;
+using COSAdmin.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COSAdmin.Areas.UserPanel.Controllers
+{
+    public class ClubUserController : Controller
+    {
+        DBEntities db = new DBEntities();
+
+        public ActionResult SelectClub()
+        {
+            List<ClubMaster> clubMasters = new List<ClubMaster>();
+
+            try
+            {
+                using (db = new DBEntities())
+                {
+                    clubMasters = db.ClubMasters.ToList();
+                }
+            }
+            catch
+            {
+
+            }
+            return View(clubMasters);
+        }
+    }
+}
